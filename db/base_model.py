6fef217665de2730a7e5b64f73060c303b71edea
Albert <amuluowin@163.com>
2023-09-14 12:38:29 +0800

    !Fix field style

--- db/base_model.py
+++ db/base_model.py
@@ -1,54 +1,54 @@
 # -*- coding: utf-8 -*-
 
-from base.di.service_location import service
+from base.di.service_location import config, service
 from base.functions import to_lower_camel, to_snake
 from base.util import date_utils
 
 
 class BaseModel:
-    STYLE_NONE = 0
-    STYLE_SNAKE = 1
-    STYLE_LOWER_CAMEL = 2
+    STYLE_NONE = 'NONE'
+    STYLE_SNAKE = 'SNAKE'
+    STYLE_LOWER_CAMEL = 'LOWER_CAMEL'
 
-    def __init__(self, key_style=STYLE_NONE, key: str = 'id') -> None:
-        self.__key_style = key_style
-        self.__key = key
+    def __init__(self, key_style=None, use_time=True, key: str = 'id') -> None:
+        self._key_style = key_style or config('model_key_style', self.STYLE_NONE)
+        self._key = key
+        self._use_time = use_time
         self._id_generator = service.id_generator
 
     @property
     def key(self) -> str:
-        return self.__key
+        return self._key
 
     @property
     def key_style(self) -> int:
-        return self.__key_style
+        return self._key_style
 
     @property
     def data(self) -> dict:
-        return self.__data
+        return self._data
 
     def load(self, data: dict) -> 'BaseModel':
-        match self.__key_style:
+        match self._key_style:
             case self.STYLE_LOWER_CAMEL:
-                self.__data = to_lower_camel(data)
+                self._data = to_lower_camel(data)
+            case self.STYLE_SNAKE:
+                self._data = to_snake(data)
             case self.STYLE_NONE:
-                self.__data = to_snake(data)
-            case self.STYLE_NONE:
-                self.__data = data
+                self._data = data
 
         return self
 
     def copy(self) -> 'BaseModel':
-        return BaseModel(self.__key_style)
+        return BaseModel(self._key_style)
 
     def to_dict(self) -> dict:
-        keep_key = self.key_style != self.STYLE_LOWER_CAMEL
-        pb_tmp = {self.key: self.data.pop(self.key, None)}
-        create_time_key = 'create_time' if keep_key and 'create_time' in self.data else 'createTime'
-        self.data.pop(create_time_key, None)
-        if not pb_tmp.get(self.key):
-            pb_tmp.update({self.key: self._id_generator.generate_id()})
-        time = date_utils.timestamp_second()
-        pb_tmp.update({create_time_key: time})
-        self.data.update({'update_time' if keep_key and 'update_time' in self.data else 'updateTime': time})
+        keep_key = self._key_style != self.STYLE_LOWER_CAMEL
+        pb_tmp = {self._key: self._data.pop(self._key, self._id_generator.generate_id())}
+        if self._use_time:
+            create_time_key = 'create_time' if keep_key and 'create_time' in self._data else 'createTime'
+            self._data.pop(create_time_key, None)
+            time = date_utils.timestamp_second()
+            pb_tmp.update({create_time_key: time})
+            self._data.update({'update_time' if keep_key and 'update_time' in self._data else 'updateTime': time})
         return pb_tmp
